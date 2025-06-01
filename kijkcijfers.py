6e4e0abf09b594a105cd34bea608729df562073b
MarijnSt <marijn.stammeleer@gmail.com>
2025-06-01 02:15:32 +0200

    test examen input

---
+++ kijkcijfers.py
@@ -0,0 +1,7 @@
+import joblib
+
+final_model_reloaded = joblib.load("kijkcijfer_model.pkl")
+
+# test in kijkcijfers.ipynb
+# neem werkende code hier over
+# output opslaan als csv en printen in console
